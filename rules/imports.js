de88e91b2061cc824fb543ad8981409849e5a785
C.T. Lin <chentsulin@gmail.com>
2016-09-01 12:39:30 +0800

    disable import/prefer-default-export

--- rules/imports.js
+++ rules/imports.js
@@ -29,6 +29,9 @@ module.exports = {
   },
 
   rules: {
-
+    // Require modules with a single export to use a default export
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
+    // Proposal - https://github.com/Yoctol/eslint-config-yoctol-base/issues/11
+    'import/prefer-default-export': 'off',
   },
 };
