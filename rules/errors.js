6edb7ffd64f1f8d0c0799fcd33a9cdab5bd5beed
tw0517tw <tw0517tw@gmail.com>
2016-12-09 17:08:37 +0800

    "comma-dangle": "functions": "ignore"
    close #51

--- rules/errors.js
+++ rules/errors.js
@@ -1,5 +1,11 @@
 module.exports = {
   rules: {
-
-  }
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'ignore',
+    }],
+  },
 };
