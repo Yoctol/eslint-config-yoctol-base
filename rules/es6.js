f6c9897f3feee5592cf6797e2d30aee30e58f285
chentsulin <chentsulin@gmail.com>
2019-01-29 17:54:43 +0800

    style(prefer-arrow-callback): set allowNamedFunctions to true

--- rules/es6.js
+++ rules/es6.js
@@ -13,9 +13,16 @@ module.exports = {
   },
 
   rules: {
+    // Require parens in arrow function arguments
     // https://eslint.org/docs/rules/arrow-parens
     'arrow-parens': ['error', 'as-needed'],
 
+    // Require using arrow functions for callbacks
+    // https://eslint.org/docs/rules/prefer-arrow-callback
+    // Proposal - https://github.com/Yoctol/eslint-config-yoctol-base/issues/87
+    'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
+
+    // Prefer destructuring from arrays and objects
     // https://eslint.org/docs/rules/prefer-destructuring
     'prefer-destructuring': [
       'error',
